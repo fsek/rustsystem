4ae7c26806579674565eeb9f693c80e24be824c7
Felix Hellborg <felix.hellborg.la@gmail.com>
2025-05-27 19:37:43 +0200

    [SERVER-CLIENT] (Initial): Setup basic communication setup between rust server (axum, using tls) and client with webassembly

---
+++ rustsystem-server/src/main.rs
@@ -0,0 +1,34 @@
+use axum::{
+    Router,
+    response::{Html, IntoResponse},
+    routing::get,
+};
+use axum_server::tls_rustls::RustlsConfig;
+use std::{fs, net::SocketAddr, str::from_utf8};
+use tower_http::services::{ServeDir, ServeFile};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/", get(index))
+        .nest_service(
+            "/www",
+            ServeDir::new("../rustsystem-client/static").append_index_html_on_directories(false),
+        )
+        .nest_service("/pkg", ServeDir::new("../rustsystem-client/pkg"));
+
+    let config = RustlsConfig::from_pem_file("certs/server.crt", "certs/server.key")
+        .await
+        .unwrap();
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8443));
+    println!("listening on {}", addr);
+    axum_server::bind_rustls(addr, config)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn index() -> Html<&'static str> {
+    Html(include_str!("../../rustsystem-client/static/index.html"))
+}
