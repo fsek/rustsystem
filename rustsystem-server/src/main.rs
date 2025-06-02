997412f655435a04b1cc31431b548e76f7308be9
Felix Hellborg <felix.hellborg.la@gmail.com>
2025-06-02 22:08:15 +0200

    [SERVER] (Update): Add cookies and fix credentials during login. The login sequence now pretty much works. After a client is sent to /login?cred={pregenerated user hash}&meeting={specific meeiting hash} the client gets redirected to /in-meeting?hash={meeting hash} and the login credentails are considered expired. The clients login is saved as a cookie, so they stay logged in for 10 hours when comming back to /in-meeting (with the same meeting hash)

--- rustsystem-server/src/main.rs
+++ rustsystem-server/src/main.rs
@@ -1,33 +1,63 @@
 use axum::{
     Extension, Json, Router,
-    extract::Query,
+    extract::{Query, State},
     http::StatusCode,
     response::{Html, IntoResponse, Redirect},
     routing::{get, post},
 };
+use axum_extra::extract::{CookieJar, cookie::Cookie};
 use axum_server::tls_rustls::RustlsConfig;
+use blake3::OUT_LEN;
 use rustsystem_proof::{Provider, RegistrationResponse, Sha256Provider, ValidationInfo};
 use rustsystem_server::session;
 use serde::Deserialize;
-use std::{net::SocketAddr, sync::Arc};
+use std::{
+    collections::{HashMap, HashSet},
+    net::SocketAddr,
+    sync::Arc,
+};
+use tokio::sync::Mutex;
 use tower_http::services::ServeDir;
 use tower_http::trace::TraceLayer;
 use tracing_subscriber::{EnvFilter, fmt};
+use uuid::Uuid;
 use zkryptium::{keys::pair::KeyPair, schemes::algorithms::BbsBls12381Sha256};
+
+use time::Duration;
 
 #[tokio::main]
 async fn main() {
-    session::gen_qr_code().unwrap();
+    fmt().with_env_filter(EnvFilter::from_default_env()).init();
+
+    // TEMPORARY FOR TESTING AND DEMOSTRATION OF SETUP
+    //session::gen_qr_code().unwrap();
 
     let keypair = Sha256Provider::generate_authentication_keys();
     let header = Header(b"Placeholder Header".to_vec());
 
-    fmt().with_env_filter(EnvFilter::from_default_env()).init();
+    let state: AppState = AppState {
+        meetings: Arc::new(Mutex::new(HashMap::new())),
+        sessions: Arc::new(Mutex::new(HashMap::new())),
+    };
+
+    let user_id = String::from("TestUser"); // This should be a randomly generated hash later on!
+    let user = User {
+        id: user_id.clone(),
+        logged_in: false,
+    };
+    let mut users = HashMap::new();
+    users.insert(user_id, user);
+    state
+        .meetings
+        .lock()
+        .await
+        .insert(String::from("TestMeeting"), users);
+    // -----------------------------------------------
 
     let app = Router::new()
         .route("/", get(index))
         .route("/login", get(voter_login))
-        .route("/authorized", get(serve_voter_page))
+        .route("/in-meeting", get(serve_voter_page))
         .route("/register", post(register))
         .route("/vote", post(validate_vote))
         .merge(rustsystem_remote::router())
@@ -38,7 +68,8 @@ async fn main() {
         .nest_service("/pkg", ServeDir::new("../rustsystem-client/pkg"))
         .layer(Extension(Arc::new(AuthenticationKeys(keypair))))
         .layer(Extension(Arc::new(header)))
-        .layer(TraceLayer::new_for_http());
+        .layer(TraceLayer::new_for_http())
+        .with_state(state);
 
     let config = RustlsConfig::from_pem_file("certs/server.crt", "certs/server.key")
         .await
@@ -63,19 +94,103 @@ pub struct AuthenticationKeys(KeyPair<BbsBls12381Sha256>);
 pub struct Header(Vec<u8>);
 
 #[derive(Deserialize)]
-struct Credentials {
-    cred: String,
+struct LoginCredentials {
+    pub cred: String,
+    pub meeting: String,
 }
 
-async fn voter_login(cred: Query<Credentials>) -> Redirect {
-    let id = cred.0.cred;
-
-    Redirect::to(&format!("/authorized?cred={id}"))
+struct User {
+    id: String,
+    logged_in: bool,
 }
 
-async fn serve_voter_page(cred: Query<Credentials>) -> Html<&'static str> {
-    println!("{}", cred.0.cred);
-    Html("")
+type Users = HashMap<String, User>;
+type ActiveMeetings = Arc<Mutex<HashMap<String, Users>>>;
+type SessionStore = Arc<Mutex<HashMap<String, String>>>;
+
+#[derive(Clone)]
+struct AppState {
+    meetings: ActiveMeetings,
+    sessions: SessionStore,
+}
+
+// Cookies expire after 10 hours
+const COOKIE_LIFETIME: Duration = Duration::hours(10);
+
+async fn voter_login(
+    jar: CookieJar,
+    cred: Query<LoginCredentials>,
+    State(state): State<AppState>,
+) -> impl IntoResponse {
+    let user_id = cred.0.cred;
+    let meeting_hash = cred.0.meeting;
+
+    if let Some(meeting_users) = state.meetings.lock().await.get_mut(&meeting_hash) {
+        if let Some(user) = meeting_users.get_mut(&user_id) {
+            if user.logged_in {
+                return (
+                    StatusCode::UNAUTHORIZED,
+                    format!("User with id {user_id} has already logged in."),
+                )
+                    .into_response();
+            } else {
+                user.logged_in = true;
+
+                let session_id = Uuid::new_v4().to_string();
+
+                state
+                    .sessions
+                    .lock()
+                    .await
+                    .insert(session_id.clone(), user_id);
+                let cookie = Cookie::build(("session_id", session_id))
+                    .path("/in-meeting")
+                    .http_only(true)
+                    .secure(true)
+                    .max_age(COOKIE_LIFETIME);
+
+                (
+                    jar.add(cookie),
+                    Redirect::to(&format!("/in-meeting?hash={meeting_hash}")),
+                )
+                    .into_response()
+            }
+        } else {
+            (
+                StatusCode::UNAUTHORIZED,
+                format!("Supplied user id {user_id} not found in meeting {meeting_hash}"),
+            )
+                .into_response()
+        }
+    } else {
+        (
+            StatusCode::UNAUTHORIZED,
+            format!("Meeting {meeting_hash} does not exist"),
+        )
+            .into_response()
+    }
+}
+
+#[derive(Deserialize)]
+struct MeetingInfo {
+    hash: String,
+}
+
+async fn serve_voter_page(
+    jar: CookieJar,
+    State(state): State<AppState>,
+    cred: Query<MeetingInfo>,
+) -> impl IntoResponse {
+    if let Some(session_cookie) = jar.get("session_id") {
+        let session_id = session_cookie.value();
+        if let Some(user_id) = state.sessions.lock().await.get(session_id) {
+            format!("Welcome back, {}!", user_id).into_response()
+        } else {
+            (StatusCode::UNAUTHORIZED).into_response()
+        }
+    } else {
+        (StatusCode::UNAUTHORIZED).into_response()
+    }
 }
 
 #[axum::debug_handler]
