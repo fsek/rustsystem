1eae7a3ab3d977bfc2c07a4855ff90bc601f20d6
Felix Hellborg <felix.hellborg.la@gmail.com>
2025-08-16 20:09:47 +0200

    [SERVER] (Update): Outline the Ballot struct for usage between server and client. This will be the data actually sent between the two

--- rustsystem-server/src/vote_auth.rs
+++ rustsystem-server/src/vote_auth.rs
@@ -1,4 +1,9 @@
-use std::collections::HashSet;
+use rand::{Fill, Rng, RngCore};
+use serde::{Deserialize, Serialize};
+use std::{
+    collections::{HashMap, HashSet},
+    io,
+};
 use tokio::sync::watch::{Receiver, Sender};
 use zkryptium::{
     keys::pair::KeyPair,
@@ -78,5 +83,75 @@ impl VoteAuthority {
 
     pub fn set_signature_expired(&mut self, signature: &BlindSignature<BbsBls12381Sha256>) {
         self.expired_signatures.insert(signature.to_bytes());
+    }
+}
+
+type VoteRoundID = u128;
+type CandidateID = u8;
+
+#[derive(Serialize, Deserialize)]
+enum VoteMethod {
+    Dichotomous,
+    Plurality,
+    RankedChoice,
+    Approval,
+    Score,
+    STAR,
+}
+
+#[derive(Serialize, Deserialize)]
+enum Choice {
+    // true for "Yes", false for "No"
+    Dichotomous(bool),
+
+    // Aka: First Past the Post (FPTP). Contains the ID of the chosen candidate.
+    Plurality(CandidateID),
+
+    // Aka: Instant Runoff. Contains (in order) the IDs of the candidates.
+    RankedChoice(Vec<CandidateID>),
+
+    // Contains the IDs of candidates approved by voter.
+    Approval(Vec<CandidateID>),
+
+    // Contains the IDs of the candidates alongside their respective scores.
+    Score(HashMap<CandidateID, u8>),
+
+    // Contains the IDs of the candidates alongside their respective scores.
+    STAR(HashMap<CandidateID, u8>),
+}
+
+#[derive(Serialize, Deserialize)]
+struct BallotValidation {
+    proof: Vec<u8>,
+    token: Vec<u8>,
+    signature: BlindSignature<BbsBls12381Sha256>,
+}
+
+#[derive(Serialize, Deserialize)]
+struct BallotMetaData {
+    method: VoteMethod,
+    protocol_version: u8,
+}
+
+// Enforce the ballot size (in bytes) to be a certain number.
+// It doesn't matter what this number is, but it must be the same for all Ballots.
+// Prevents some sophisticated network analysis attacks
+const BALLOT_SIZE: usize = 1024;
+
+#[derive(Serialize, Deserialize)]
+struct Ballot {
+    metadata: BallotMetaData,
+    choice: Option<Choice>, // None for blank vote
+    validation: BallotValidation,
+    _padding: Vec<u8>,
+}
+impl Ballot {
+    pub fn resize(&mut self) -> io::Result<()> {
+        let bytes = serde_json::to_vec(&self)?;
+
+        self._padding.resize(BALLOT_SIZE - bytes.len(), 0);
+        // Randomize to avoid determenistic compression
+        rand::rng().fill_bytes(&mut self._padding);
+        Ok(())
     }
 }
