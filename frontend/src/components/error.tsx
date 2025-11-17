146502ea4e379e5d4005074ab8f9e3f56de3f4f2
maffe <manfred.malmros@gmail.com>
2025-11-17 17:44:53 +0100

    remove bad button

--- frontend/src/components/error.tsx
+++ frontend/src/components/error.tsx
@@ -120,12 +120,6 @@ function ErrorPage({ error }: ErrorProps) {
 						>
 							Gå tillbaka
 						</button>
-						<a
-							href={"/"}
-							className="inline-flex items-center justify-center rounded-xl bg-red-600 px-4 py-2 text-sm font-medium text-white shadow-sm transition hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-400"
-						>
-							Go to start
-						</a>
 						<button
 							type="button"
 							onClick={exportJSON}
